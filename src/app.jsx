d288d201b74c1b8f014abc6287ac2adb81fe2135
keundeok2 <keundeok2@gmail.com>
2021-03-01 14:24:39 +0900

    most popular list's title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1> hello world! </h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyARkgfjtA5H3lwpNSYaK3zipugm1_l16kk',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
